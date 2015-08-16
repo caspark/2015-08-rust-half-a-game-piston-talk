58617479ac468f7d22724b0d74921c0a8ee36ce0
Caspar Krieger <caspar@asparck.com>
2015-08-16 22:30:19 +1000

    Split into intro + main game

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,13 @@ use sprite::{Animation, Blink, Ease, EaseFunction, FadeIn, FadeOut, MoveBy, Rota
 use ai_behavior::{ Action, Behavior, Sequence, Wait, WaitForever, While };
 use uuid::Uuid;
 
+const WIDTH: u32 = 300;
+const HEIGHT: u32 = 300;
+
 fn main() {
-    let (width, height) = (300, 300);
     let opengl = OpenGL::V3_2;
     let window: PistonWindow =
-        WindowSettings::new("piston: sprite", (width, height))
+        WindowSettings::new("piston: sprite", (WIDTH, HEIGHT))
         .exit_on_esc(true)
         .opengl(opengl)
         .build()
@@ -27,7 +29,11 @@ fn main() {
     let assets: PathBuf = find_folder::Search::ParentsThenKids(3, 3)
         .for_folder("assets").unwrap();
 
-    let mut scene: Scene<Texture<Resources>> = Scene::new();
+    run_intro(&window, &assets);
+    run_game(&window, &assets);
+}
+
+fn run_intro(window: &PistonWindow, assets: &PathBuf) {
     let tex: Rc<Texture<Resources>> = Rc::new(Texture::from_path(
             &mut *window.factory.borrow_mut(),
             assets.join("rust-sydney.png"),
@@ -35,43 +41,48 @@ fn main() {
             &TextureSettings::new()
         ).unwrap());
     let mut sprite: Sprite<Texture<Resources>> = Sprite::from_texture(tex.clone());
-    sprite.set_position(width as f64 / 2.0, height as f64 / 2.0);
+    sprite.set_position(WIDTH as f64 / 2.0, HEIGHT as f64 / 2.0);
 
+    let mut scene: Scene<Texture<Resources>> = Scene::new();
     let id: Uuid = scene.add_child(sprite);
 
-    // Run a sequence or animations.
+    // Run a sequence of animations, using ai_behavior + animations from the sprite crate
     let seq: Behavior<Animation> = Sequence(vec![
         Action(Ease(EaseFunction::CubicOut, Box::new(ScaleTo(2.0, 0.5, 0.5)))),
-        Action(Ease(EaseFunction::BounceOut, Box::new(MoveBy(1.0, 0.0, 100.0)))),
-        Action(Ease(EaseFunction::ElasticOut, Box::new(MoveBy(2.0, 0.0, -100.0)))),
-        Action(Ease(EaseFunction::BackInOut, Box::new(MoveBy(1.0, 0.0, -100.0)))),
+        Action(Ease(EaseFunction::BounceOut, Box::new(MoveBy(1.0, 0.0, 80.0)))),
         Wait(0.5),
-        Action(Ease(EaseFunction::ExponentialInOut, Box::new(MoveBy(1.0, 0.0, 100.0)))),
-        Action(Blink(1.0, 5)),
-        While(Box::new(WaitForever), vec![
-            Action(Ease(EaseFunction::QuadraticIn, Box::new(FadeOut(1.0)))),
-            Action(Ease(EaseFunction::QuadraticOut, Box::new(FadeIn(1.0)))),
-        ]),
+        Action(FadeOut(0.3)),
     ]);
     scene.run(id, &seq);
 
-    // This animation and the one above can run in parallel.
-    let rotate: Behavior<Animation> = Action(Ease(EaseFunction::ExponentialInOut,
-        Box::new(RotateTo(2.0, 360.0))));
+    // run another animation in parallel (could also use ai_behavior to do this instead)
+    let rotate: Behavior<Animation> =
+        Action(Ease(EaseFunction::QuarticInOut, Box::new(RotateTo(2.5, 1080.0))));
     scene.run(id, &rotate);
 
-    println!("Press any key to pause/resume the animation!");
-
-    for e in window { // e is a PistonWindow
-        scene.event(&e);
+    for e in window.clone() {
+        scene.event(&e); // updates animations
 
         e.draw_2d(|c, g| {
             clear([1.0, 1.0, 1.0, 1.0], g);
             scene.draw(c.transform, g);
         });
-        if let Some(_) = e.press_args() {
-            scene.toggle(id, &seq);
-            scene.toggle(id, &rotate);
+        if Some(Button::Keyboard(Key::Space)) == e.press_args() ||
+                Some(Button::Keyboard(Key::Return)) == e.press_args() ||
+                scene.running() == 0 {
+            return;
         }
+    }
+}
+
+fn run_game(window: &PistonWindow, assets: &PathBuf) {
+    for e in window.clone() {
+        println!("In inner loop now");
+
+        e.draw_2d(|c, g| {
+            clear([0.0, 1.0, 1.0, 1.0], g);
+            //TODO do something interesting here
+            // scene.draw(c.transform, g);
+        });
     }
 }
